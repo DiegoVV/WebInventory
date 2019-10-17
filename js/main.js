f7f5502429da6430051310e111997f046d8aada2
Diego <diegoolegalebom@hotmail.com>
2019-10-17 11:07:17 -0300

    Delete button works, and I have learned the proper way of creating buttons to append into HTML

--- js/main.js
+++ js/main.js
@@ -32,19 +32,48 @@ request.send();*/
 }*/
 
 function showProducts() {
+    $("table tr").remove();
+    var trow = document.createElement("tr");
+    trow.className = "tags";
+    trow.innerHTML = "<td>Name</td>" +
+        "<td>Description</td>" +
+        "<td>Price</td>" +
+        "<td>Category</td>" +
+        "<td>Options</td>"
+    table.appendChild(trow);
     db.allDocs({
         include_docs: true,
-        descending: true
+        descending: false
     }, function (err, doc) {
         //redrawTodosUI(doc.rows);
         doc.rows.forEach(function (object) {
-            console.log(object);
             var tr = document.createElement("tr");
             tr.innerHTML = "<td>" + object.doc.name + "</td>" +
                 "<td>" + object.doc.desc + "</td>" +
                 "<td>R$" + object.doc.price + "</td>" +
-                "<td>" + object.doc.category + "</td>" +
-                "<td>" + "</button>" + "<button class='buttons' onclick='editItem()'>" + "Edit" + "</button>" + "<button class='buttons' onclick='deleteItem(this)'>" + "Delete" + "</td>"
+                "<td>" + object.doc.category + "</td>";
+            var td = document.createElement('td');
+            var btn = document.createElement('button');
+            btn.innerHTML = "Edit";
+            btn.type = "button";
+            btn.className = "buttons";
+            btn.onclick = (function (object) {
+                return function () {
+                    editItem(object.doc);
+                }
+            })(object);
+            td.appendChild(btn);
+            btn = document.createElement('button');
+            btn.innerHTML = "Delete";
+            btn.type = "button";
+            btn.className = "buttons";
+            btn.onclick = (function (object) {
+                return function () {
+                    deleteItem(object.doc);
+                }
+            })(object);
+            td.appendChild(btn);
+            tr.appendChild(td);
             table.appendChild(tr);
         });
     });
@@ -66,17 +95,11 @@ function addItem() {
 }
 
 function saveItem(o) {
-    //store the form data in data.json
+    //store the form data in the database
     var formName = document.getElementsByName('name')[0].value;
     var formDesc = document.getElementsByName('desc')[0].value;
     var formPrice = parseFloat(document.getElementsByName('price')[0].value).toFixed(2);
     var formCategory = document.getElementsByName('category')[0].value;
-    /*var obj = '{' +
-        '"name" : ' + formName + ',' +
-        '"desc"  : ' + formDesc + ',' +
-        '"price" : ' + formPrice + ',' +
-        '"category" : ' + formCategory + ',' +
-        '}';*/
     var obj = {
         _id: new Date().toISOString(),
         name: formName,
@@ -95,7 +118,8 @@ function saveItem(o) {
 }
 
 function deleteItem(o) {
-    //remove delete the information from data.json, then cancel it
+    //remove it from the database
+    console.log(o);
     db.remove(o);
 }
 
@@ -107,3 +131,5 @@ function cancelItem(o) {
         addingItem = false;
     }
 }
+
+function editItem(o) {}
