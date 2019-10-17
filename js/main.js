287cb929e97b31c386c4373069b87404f85612b1
Diego <diegoolegalebom@hotmail.com>
2019-10-17 09:37:40 -0300

    The database gets updated with new items and shows them when you open up the website again. Working on deleting items now

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ var db = new PouchDB('inventoryItems');
 var remoteCouch = false;
 
 var table = document.getElementById("inv");
-var howManyItems = 0;
 var addingItem = false;
+
+showProducts();
+
+db.changes({
+    since: 'now',
+    live: true
+}).on('change', showProducts);
 
 /*var request = new XMLHttpRequest();
 request.open("GET", "../data.json");
@@ -15,7 +21,6 @@ request.send();*/
 
 /*function append_json(data) {
     data.forEach(function (object) {
-        howManyItems += 1;
         var tr = document.createElement("tr");
         tr.innerHTML = "<td>" + object.name + "</td>" +
             "<td>" + object.desc + "</td>" +
@@ -26,10 +31,23 @@ request.send();*/
     });
 }*/
 
-function showProducts(){
-    db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    redrawTodosUI(doc.rows);
-  });
+function showProducts() {
+    db.allDocs({
+        include_docs: true,
+        descending: true
+    }, function (err, doc) {
+        //redrawTodosUI(doc.rows);
+        doc.rows.forEach(function (object) {
+            console.log(object);
+            var tr = document.createElement("tr");
+            tr.innerHTML = "<td>" + object.doc.name + "</td>" +
+                "<td>" + object.doc.desc + "</td>" +
+                "<td>R$" + object.doc.price + "</td>" +
+                "<td>" + object.doc.category + "</td>" +
+                "<td>" + "</button>" + "<button class='buttons' onclick='editItem()'>" + "Edit" + "</button>" + "<button class='buttons' onclick='deleteItem(this)'>" + "Delete" + "</td>"
+            table.appendChild(tr);
+        });
+    });
 }
 
 function addItem() {
@@ -40,19 +58,18 @@ function addItem() {
             "<td>" + "<textarea name='desc'>" + "</textarea>" + "</td>" +
             "<td>R$" + "<input type='number' name='price' min='0.00' step='0.01'>" + "</td>" +
             "<td>" + "<select name='category'>" + "<option value='book'>Book</option>" + "<option value='food'>Food</option>" + "<option value='furniture'>Furniture</option>" + "<option value='vehicle'>Vehicle</option>" + "<option value='eletronic'>Eletronic</option>" + "</select>" + "</td>" +
-            "<td>" + "<button class='buttons' onclick='saveItem()'>" + "Save" + "</button>" + "<button class='buttons' onclick='cancelItem(this)'>" + "Cancel" + "</button>" + "</td>" +
+            "<td>" + "<button class='buttons' onclick='saveItem(this)'>" + "Save" + "</button>" + "<button class='buttons' onclick='cancelItem(this)'>" + "Cancel" + "</button>" + "</td>" +
             "</form>";
         table.appendChild(tr);
-        howManyItems += 1;
         addingItem = true;
     }
 }
 
-function saveItem() {
+function saveItem(o) {
     //store the form data in data.json
     var formName = document.getElementsByName('name')[0].value;
     var formDesc = document.getElementsByName('desc')[0].value;
-    var formPrice = document.getElementsByName('price')[0].value;
+    var formPrice = parseFloat(document.getElementsByName('price')[0].value).toFixed(2);
     var formCategory = document.getElementsByName('category')[0].value;
     /*var obj = '{' +
         '"name" : ' + formName + ',' +
@@ -66,11 +83,12 @@ function saveItem() {
         desc: formDesc,
         price: formPrice,
         category: formCategory
-        };
-    db.put(obj).then(function(result){
-        console.log('Successfully add an item!');
+    };
+    cancelItem(o);
+    db.put(obj).then(function (result) {
+        console.log('Successfully added an item!');
         console.log(result);
-    }).catch(function(err){        
+    }).catch(function (err) {
         console.log('Something went wrong, item not added');
         console.log(err);
     });
@@ -78,7 +96,7 @@ function saveItem() {
 
 function deleteItem(o) {
     //remove delete the information from data.json, then cancel it
-    cancelItem(o);
+    db.remove(o);
 }
 
 function cancelItem(o) {
@@ -86,7 +104,6 @@ function cancelItem(o) {
     if (addingItem) {
         var p = o.parentNode.parentNode;
         p.parentNode.removeChild(p);
-        howManyItems -= 1;
         addingItem = false;
     }
 }
